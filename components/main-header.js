13f41016d26ce03b294abdca0de2e38489dc9dc8
deepakdroi <deepak.mahto706@gmail.com>
2024-03-16 16:34:39 +0530

    header added and styled

---
+++ components/main-header.js
@@ -0,0 +1,25 @@
+import Link from "next/link";
+
+import classes from "./main-header.module.css";
+import logoImg from "@/assets/logo.png";
+
+export default function MainHeader() {
+  return (
+    <header className={classes.header}>
+      <Link className={classes.logo} href="/">
+        <img src={logoImg.src} alt="A plate with food in it." />
+        Next Level food
+      </Link>
+      <nav className={classes.nav}>
+        <ul>
+          <li>
+            <Link href="/meal">Brouse Meals</Link>
+          </li>
+          <li>
+            <Link href="/community">Foodies Community</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
