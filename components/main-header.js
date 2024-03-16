1ed84d50f9ccce63cc204377168e32c6faf520ea
deepakdroi <deepak.mahto706@gmail.com>
2024-03-16 16:39:31 +0530

    minor bug was fixed

--- components/main-header.js
+++ components/main-header.js
@@ -13,7 +13,7 @@ export default function MainHeader() {
       <nav className={classes.nav}>
         <ul>
           <li>
-            <Link href="/meals">Brouse Meals</Link>
+            <Link href="/meals">Browse Meals</Link>
           </li>
           <li>
             <Link href="/community">Foodies Community</Link>
