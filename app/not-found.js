a9b6b02952e566db56d769167e0fc3b4bc8e3515
deepakdroi <deepak.mahto706@gmail.com>
2024-03-17 21:29:42 +0530

    Page not found was handled

---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>Page not found</h1>
+      <p>Unfortunately we couldnot find the page you requested for.</p>
+    </main>
+  );
+}
