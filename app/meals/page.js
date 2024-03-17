aac6c00b678e36a814c96de7fa831daf9e374871
deepakdroi <deepak.mahto706@gmail.com>
2024-03-17 13:09:39 +0530

    Contents of meals page was updated and comonents were set up to render all the meals witn currently no data to render

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,9 +1,26 @@
 import Link from "next/link";
 
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
+
 export default function MealsPage() {
   return (
-    <main>
-      <h1 style={{ color: "white", textAlign: "center" }}>Meals</h1>
-    </main>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meal, Created (" ")
+          <span className={classes.highlight}>by You</span>
+        </h1>
+        <p>
+          Choose your favorite recipes and cook it yourself. Its easy and fun.
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share your favorite recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
   );
 }
