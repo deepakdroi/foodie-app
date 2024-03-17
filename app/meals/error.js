0a0c671e136bcb31206d0dfefdc76e2b0eba375e
deepakdroi <deepak.mahto706@gmail.com>
2024-03-17 21:22:12 +0530

    Error handling was done

---
+++ app/meals/error.js
@@ -0,0 +1,10 @@
+"use client";
+
+export default function Error() {
+  return (
+    <main className="error">
+      <h1>An Error Has Occured</h1>
+      <p>Something went wring while loading meals. Please try again.</p>
+    </main>
+  );
+}
