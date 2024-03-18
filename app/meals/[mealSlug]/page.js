aa9744fb46f6fa7db3140e2e9baf89c34b82deb2
deepakdroi <deepak.mahto706@gmail.com>
2024-03-18 09:00:01 +0530

    Added content to meal details and populated by fetching data from db

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,8 +1,33 @@
-export default function SomeSlug({ params }) {
+import Image from "next/image";
+
+import classes from "./page.module.css";
+import { getMeal } from "@/lib/meals";
+
+export default async function MealsDEtailsPage({ params }) {
+  const meal = await getMeal(params.mealSlug);
+  meal.instructions = meal.instructions.replace(/\n/g, "<br/>");
   return (
-    <main>
-      <h1 style={{ color: "red", textAlign: "center" }}> Some Slug</h1>
-      <h2 style={{ color: "red", textAlign: "center" }}>{params.mealSlug}</h2>
-    </main>
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main className={classes.main}>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        ></p>
+      </main>
+    </>
   );
 }
