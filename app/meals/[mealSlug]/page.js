b8f67319bb3438ee55be49ebe6fd997b6959309b
deepakdroi <deepak.mahto706@gmail.com>
2024-03-20 18:41:54 +0530

    Small bug fix

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -16,7 +16,7 @@ export default async function MealsDEtailsPage({ params }) {
     <>
       <header className={classes.header}>
         <div className={classes.image}>
-          <Image src={meal.image} fill />
+          <Image src={meal.image} alt={`${meal.title} image`} fill />
         </div>
         <div className={classes.headerText}>
           <h1>{meal.title}</h1>
