6984e62631a2163ff9132c8aa3565a5850387429
deepakdroi <deepak.mahto706@gmail.com>
2024-03-21 23:50:44 +0530

    Some basic validation was handled and error was thrown

---
+++ app/meals/share/error.js
@@ -0,0 +1,10 @@
+"use client";
+
+export default function Error() {
+  return (
+    <main className="error">
+      <h1>An Error Has Occured</h1>
+      <p>Failed to submit meal. Invalid input.</p>
+    </main>
+  );
+}
